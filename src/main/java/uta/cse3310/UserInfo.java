476773bb2dca99f65e282d5092597cd3fbd95978
Joseph Shadrick <Joseph.Shadrick@mavs.uta.edu>
2022-03-28 14:00:21 -0500

    Added UserInfo class

---
+++ src/main/java/uta/cse3310/UserInfo.java
@@ -0,0 +1,18 @@
+package uta.cse3310;
+
+public class UserInfo
+{
+	public int addcoin()
+	{
+		return 0;
+	}
+
+	public void change_password()
+	{
+	}
+
+	private char user_name;
+	private int player_id;
+	private String user_password;
+	private int user_coin;
+}
